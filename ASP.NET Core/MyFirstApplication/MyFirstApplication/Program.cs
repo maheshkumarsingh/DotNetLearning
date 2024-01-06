f96f31f6a64239d2963a43649e587b47b4b94325
Mahesh Singh <2100227@cognizant.com>
2024-01-06 18:50:06 +0530

    headers

--- ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
+++ ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    //context.Response.StatusCode = 400;
+    context.Response.Headers["MyKey"] = "my value";
+    await context.Response.WriteAsync("Hello Mahesh");
+    await context.Response.WriteAsync("Kumar Singh");
+});
 
 app.Run();
