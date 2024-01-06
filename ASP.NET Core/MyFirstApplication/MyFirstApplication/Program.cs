20306da7c13b8c49a45099a8caeda171aad9362e
Mahesh Singh <2100227@cognizant.com>
2024-01-06 20:14:44 +0530

    query string

--- ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
+++ ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
@@ -8,11 +8,30 @@ app.Run(async (HttpContext context) =>
     context.Response.Headers["Server"] = "Mahesh ka Sever";
 
     string path = context.Request.Path;
-    string method = context.Request.Method;
+    //string method = context.Request.Method;
+
+
+    //http://localhost:5279/?id=1&name=Mahesh
+    if (context.Request.Method == HttpMethod.Get.ToString())
+    {
+        if(context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    } 
+    if(context.Request.Method == HttpMethod.Get.ToString())
+    {
+        if(context.Request.Query.ContainsKey("name"))
+        {
+            string name = context.Request.Query["name"];
+            await context.Response.WriteAsync($"<p>{name}</p>");
+        }
+    }
+    
     context.Response.Headers["Content-Type"] = "text/html";
     await context.Response.WriteAsync("<h1>Hello Mahesh</h1>");
     await context.Response.WriteAsync("<h2> Kumar Singh</h2>");
-    await context.Response.WriteAsync($"<p>{path} {method}</p>");
 });
 
 app.Run();
