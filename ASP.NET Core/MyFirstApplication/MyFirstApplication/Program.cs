2ac0c076f4d48e568ecb4832cec69f94028847d4
Mahesh Singh <2100227@cognizant.com>
2024-01-06 19:20:47 +0530

    added content type

--- ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
+++ ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
@@ -5,8 +5,10 @@ app.Run(async (HttpContext context) =>
 {
     //context.Response.StatusCode = 400;
     context.Response.Headers["MyKey"] = "my value";
-    await context.Response.WriteAsync("Hello Mahesh");
-    await context.Response.WriteAsync("Kumar Singh");
+    context.Response.Headers["Server"] = "Mahesh ka Sever";
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("<h1>Hello Mahesh</h1>");
+    await context.Response.WriteAsync("<h2> Kumar Singh</h2>");
 });
 
 app.Run();
