9a6bc2391dd28851753faddf0b8c49f13ccaa072
Mahesh Singh <2100227@cognizant.com>
2024-01-06 19:59:13 +0530

    request

--- ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
+++ ASP.NET Core/MyFirstApplication/MyFirstApplication/Program.cs
@@ -6,9 +6,13 @@ app.Run(async (HttpContext context) =>
     //context.Response.StatusCode = 400;
     context.Response.Headers["MyKey"] = "my value";
     context.Response.Headers["Server"] = "Mahesh ka Sever";
+
+    string path = context.Request.Path;
+    string method = context.Request.Method;
     context.Response.Headers["Content-Type"] = "text/html";
     await context.Response.WriteAsync("<h1>Hello Mahesh</h1>");
     await context.Response.WriteAsync("<h2> Kumar Singh</h2>");
+    await context.Response.WriteAsync($"<p>{path} {method}</p>");
 });
 
 app.Run();
